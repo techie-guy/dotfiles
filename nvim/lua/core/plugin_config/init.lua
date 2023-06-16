dbdc5e7cf5d33378c3a5b9aecb7d3615d7040a51
techie-guy <devtechieguy@gmail.com>
2023-06-16 21:12:43 +0530

    nvim: Configure LSP

--- nvim/lua/core/plugin_config/init.lua
+++ nvim/lua/core/plugin_config/init.lua
@@ -1,9 +1,14 @@
 require("core.plugin_config.lualine")
+require("core.plugin_config.alpha")
+require("core.plugin_config.barbar")
 require("core.plugin_config.nvim-tree")
+
 require("core.plugin_config.telescope")
 require("core.plugin_config.treesitter")
-require("core.plugin_config.bufferline")
 require("core.plugin_config.colorizer")
 require("core.plugin_config.indent-blankline")
 require("core.plugin_config.nvim-autopairs")
-require("core.plugin_config.alpha")
+
+-- LSP
+--require("core.plugin_config.lspconfig")
+--require("core.plugin_config.completions")
